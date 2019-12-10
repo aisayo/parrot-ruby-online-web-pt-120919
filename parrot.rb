3769aff098f505ec5cb51c90e6ee305054813bdf
Aysan Isayo <aisayo@icloud.com>
2019-12-10 17:54:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -9,7 +9,7 @@
 #methods can also accept arguments: data being utilized in the scope of that method 
 #use placeholders for arguments: variables 
 
-def parrot(phrase )
-  puts "Squawk!"
+def parrot(phrase = "Squawk!" )
+  puts phrase
 end 
 
