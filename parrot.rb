3a8583594c798ebae773ebad13936c032ca16988
Aysan Isayo <aisayo@icloud.com>
2019-12-10 17:44:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -9,13 +9,11 @@
 #methods can also accept arguments: data being utilized in the scope of that method 
 #use placeholders for arguments: variables 
 
-name = "aysan"
-name = "esteban"
-name = "catherine"
+
 
 def example(name)
   puts name 
 end 
 
-example(name)
+example("aysan")
 
