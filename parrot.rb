b8e441ed8e8397d0a065713d90e46205208b670a
Aysan Isayo <aisayo@icloud.com>
2019-12-10 17:36:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+
+#what are arguments
+
+#methods are boxes that encapsulate some type of logic
+#methods can also accept arguments: data being utilized in the scope of that method 
+
+
+def example
+  name = "aysan"
+end 
